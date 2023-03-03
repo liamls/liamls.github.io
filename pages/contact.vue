355d142783602b0a5e6b4238dd4d111bd7b05d95
LE STRAT Liam <liam.lestrat@orange.com>
2023-03-03 14:15:37 +0100

    fix

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="container">
-    <h1>Mail : lestratliam@gmail.com</h1>
+    <div class="window">
+      <div class="title-bar">
+        <div class="title-bar-controls">
+          <button aria-label="Close"></button>
+          <button aria-label="Minimize"></button>
+          <button aria-label="Zoom"></button>
+        </div>
+      </div>
+      <div class="content">
+        <p>Mail : lestratliam@gmail.com</p>
+        <p>Github : @liamls</p>
+        <p>Linkedin : Liam Le Strat</p>
+      </div>
+    </div>
   </div>
 </template>
